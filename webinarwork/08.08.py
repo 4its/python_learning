57bd83ddc4999cad3ccba09d7c11e0983d5b6bfb
Goerge Egiazaryan <egiazaryan@gmail.com>
2022-08-11 17:16:44 +0300

    Объекты и классы. Работа(повторение) дома, самостоятельно.

--- webinarwork/08.08.py
+++ webinarwork/08.08.py
@@ -51,46 +51,46 @@
 # car_a.stop()
 
 
-class Car:
-    # create class attributes
-    car_count = 0
-    qqq = "Hello"
-    # create class methods
-    def __init__(self, name):
-        self.name = name
-        Car.car_count += 1
-        print(Car.car_count)
-        qqq = "Hello again"
-
-    def start(self, make, model):
-        print ("Engine started")
-        self.make  = make
-        self.model = model
-        qqq = "Hello again again"
-        # Car.car_count += 1
-
-    def get_name(self):
-        print(self.name)
-
-    def stop(self):
-        print("Engine switched off")
-
-
-car_a = Car("Corrola")
-car_a.start("Toyota", 2015)
-print(car_a.name)
-print(car_a.car_count)
-print(car_a.qqq)
-
-car_b = Car("City")
-car_b.start("Honda", 2013)
-print(car_b.name)
-print(car_b.car_count)
-print(car_b.qqq)
-
-
-
-
+# class Car:
+#     # create class attributes
+#     car_count = 0
+#     qqq = "Hello"
+#     # create class methods
+#     def __init__(self, name):
+#         self.name = name
+#         Car.car_count += 1
+#         print(Car.car_count)
+#         qqq = "Hello again"
+#
+#     def start(self, make, model):
+#         print ("Engine started")
+#         self.make  = make
+#         self.model = model
+#         qqq = "Hello again again"
+#         # Car.car_count += 1
+#
+#     def get_name(self):
+#         print(self.name)
+#
+#     def stop(self):
+#         print("Engine switched off")
+#
+#
+# car_a = Car("Corrola")
+# car_a.start("Toyota", 2015)
+# print(car_a.name)
+# print(car_a.car_count)
+# print(car_a.qqq)
+#
+# car_b = Car("City")
+# car_b.start("Honda", 2013)
+# print(car_b.name)
+# print(car_b.car_count)
+# print(car_b.qqq)
+#
+#
+#
+#
 
 
 #
@@ -105,3 +105,80 @@ print(car_b.qqq)
 # print(a.area())
 # #error#Rectangle.area()
 
+    #   HOME WORK
+
+# class Car:
+#     # create class attributes
+#     name = "c200"
+#     make = "mercedez"
+#     model = 2008
+#
+#     # create class methods
+#     def get_name(self):
+#         print(self.name)
+#
+#     def get_hp(self):
+#         self.hp = 210
+#         # print(self.hp)
+#
+#     def start(self):
+#         print ("Engine started")
+#     def stop(self):
+#         print ("Engine switched off")
+
+class Car:
+    # create class attributes
+    car_count = 0
+    test1 = 'Hello'
+    # create class methods
+    def __init__(self, name):
+        self.name = name
+        test1 = 'Hello again'
+        Car.car_count += 1
+        print('Car_count is:',Car.car_count)
+
+
+    def start(self, make, model):
+        print ("Engine started")
+        self.make = make
+        self.model = model
+        test1 = 'Hello again again'
+        # Car.car_count += 1
+
+    def get_name(self):
+        print(self.name)
+
+
+car_a = Car("Corrola")
+car_a.start("Toyota", 2015)
+print(car_a.name)
+print(car_a.test1)
+
+car_b = Car("Civic" )
+car_b.start("Honda", 2013)
+print(car_b.name)
+print(car_b.test1)
+
+
+
+
+# class Rectangle:
+#     def __init__(self, width, height):
+#         self.width = width
+#         self.height = height
+#     def area(self):
+#         return self.width * self.height
+# a=Rectangle(10,20)
+# print(a.area()) #error## Rectangle.area()
+
+print('\n\n')
+
+
+class Car2:
+    message1 = 'Engine started'
+
+    def start(self):
+        message2 = "Car started"
+        return message2
+car_a2 = Car2()
+print(car_a2.start())
