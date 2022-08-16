4b65c82560609362c25a80351a45516278fc115d
Goerge Egiazaryan <egiazaryan@gmail.com>
2022-08-16 12:34:42 +0300

    Работа на занятии 11.08.2022 part 2 Выполнение домашнего задания 2.1

---
+++ HomeWorks/Module_2/HW_2.1_Classes_&_Objects_EgiazaryanGP.py
@@ -0,0 +1,214 @@
+from random import randint as rnd
+
+print('=== Task #1 класс – прямоугольник ===')
+# Написать класс – прямоугольник. Учитывая следующие рекомендации:
+# - создайте метод __init__(), внутри которого будут определены два динамических свойства:
+#   ширина, длина. Начальные значения свойств берутся из входных параметров метода.
+# -	создайте метод print() – вывод на экран параметров прямоугольника – ширина-высота,
+# -	создайте метод PrintP  - который возвращает периметр прямоугольника
+# -	создайте метод PrintS  - который возвращает площадь прямоугольника
+# -	создайте метод __eq__(self, other) сравнивает два прямоугольника на основании площади, выводит на экран результат сравнения
+
+# class Rectangle:
+#
+#     def __init__(self, w, l):
+#         self.width, self.length = w, l
+#
+#     def prnt(self):         # создайте метод print() – вывод на экран параметров прямоугольника – ширина-высота
+#         print('Width is:', self.width)
+#         print('Length is:', self.length)
+#
+#     def prntP(self):
+#         return (self.width + self.length) * 2
+#
+#     def prntS(self):
+#         return (self.width * self.length)
+#
+#     def __eq__(self, other):
+#         if self.prntS() == other.prntS():
+#             print('Rectangles have same area')
+#         else:
+#             print('Different area of rectangles')
+#
+#
+# rect_a = Rectangle(3, 5)        # rectangle #1
+# rect_b = Rectangle(3, 5)        # rectangle #2
+#
+# print("Call method print()")
+# rect_a.prnt()
+# print("\nCall method for perimetr =>")
+# print('Perimetr of rectangle is:', rect_a.prntP() )
+# print("\nCall method area =>")
+# print('Rectangle area is:',rect_a.prntS())
+# print("\nCall method __eq__ =>")
+# rect_a == rect_b
+
+print('\n=== Task #2 класс – списки целых чисел ===')
+
+# Написать класс – списки целых чисел. Учитывая следующие рекомендации:
+# - создайте метод __init__(), внутри которого будут определен один параметр: размер списка.
+#           Начальные значения свойства берутся из входных параметров метода.
+# -	создайте метод InputData позволяющий задать данные списка пользователем
+# -	создайте метод InputDataRandom заполняющий список с помощью датчика случайных чисел
+# -	создайте метод print() – вывод на экран содержимого списка
+# -	создайте метод FindValue  - который возвращает список индексов для искомого элемента
+# -	создайте метод DelValue  - который удаляет из списка искомый элемент.
+# -	создайте метод FindMax- который возвращает максимальное значение из списка.
+# -	создайте метод __add__(self, other) который выполняет сложение двух списков одинаковой длина поэлементно
+
+# class IntList:
+#
+#     a,b = 0, 10        # define area for random generator set
+#     inlst = []
+#     def __init__(self, n):      # init with defining length of list
+#         self.n = n
+#
+#     def prnt(self):             # метод print()
+#         if len(self.inlst) == 0:
+#             print('List is empty, please fill it if you want to see some values')
+#         else:
+#             print('Our list is:', self.inlst)
+#
+#     def InputData(self):        # метод InputData
+#         self.inlst = [(int(input('Enter the element of list(int only):'))) for i in range(self.n)]
+#
+#     def InputDataRandom(self):  # метод InputDataRandom
+#         self.inlst = [rnd(self.a, self.b) for i in range(self.n)]
+#
+#     def FindValue(self, fv):    # метод FindValue
+#         tmp = [i for i,item in enumerate(self.inlst) if item == fv]
+#
+#         if len(tmp) == 0:       # Checking list that it not empty
+#             print("There is no value", fv, "in list.")
+#         else:
+#             print('Found value', fv, 'in next indexes:', tmp)
+#
+#     def DelOneValue(self, fv):      # метод DelValue (will delete first entered value)
+#         if fv in self.inlst:
+#             self.inlst.remove(fv)
+#             # print('List after delete FIRST',fv , 'value:', self.inlst)     # Debugging print
+#         else:
+#             print('Value', fv, 'not in list.')
+#
+#     def DelAllValues(self, fv):     # метод DelValue (will delete ALL entered value)
+#         if fv in self.inlst:
+#             tmp = [i for i, item in enumerate(self.inlst) if item == fv]
+#             for item in reversed(tmp):
+#                 self.inlst.pop(item)
+#         else:
+#             print('Value', fv, 'not in list.')
+#
+#     def FindMax(self):      # метод FindMax
+#         return max(self.inlst)
+#
+#     def __add__(self, other):
+#         if len(self.inlst) == len(other.inlst):
+#             tmp = [(self.inlst[i]+other.inlst[i]) for i in range(len(self.inlst))]
+#             return tmp
+#         else:
+#             print("We CAN'T summ selected list becose they have different length")
+#
+#
+# print('Lets start with chose of length of list!')
+# n = int(input('Please, enter length of list: '))        #defining length of list
+#
+# ls = IntList(n)         # creating object from class with length n
+#
+# print("Now we must fill our list. Let's choose how we will do that: \n  1 - Manual input\n  2 - With RandomInt function")
+# choise = int(input('Enter number of you choose: '))
+# if choise == 1:
+#     ls.InputData()
+# elif choise == 2:
+#     ls.InputDataRandom()
+# else:
+#     print('Entered wrong number of choose! Please restart the program')
+#
+# print('\n=> Call method prnt to see what we have in our list:')
+# ls.prnt()
+#
+# print('\n=> Call method FindMax to see max value in list:')
+# print('Max value is:', ls.FindMax())
+#
+# print('\n=> Call method FindValue to find indexes of value that we want to find:')
+# fv = int(input('Enter value that you want find in list: '))
+# ls.FindValue(fv)
+#
+# print('\n=> Now we want call delete method. Lets choose with one we want to use:')
+# print('1 - Delete FIRST finded value\n2 - Delete ALL finded values')
+# choise = int(input('Enter number of you choose: '))
+# fv = int(input('Enter value that you want find and delete in list: '))
+# if choise == 1:
+#     ls.DelOneValue(fv)
+#     print('List after delete FIRST value', fv,'in list is:')
+#     ls.prnt()
+# elif choise == 2:
+#     ls.DelAllValues(fv)
+#     print('List after delete ALL values', fv, 'in list is:')
+#     ls.prnt()
+# else:
+#     print('Entered wrong number of choose! Please restart the program')
+#
+# print('\n=> Now we want call __add__ method. To do it we create two randomly generated lists.')
+# i = int(input('Please, enter length of first  list: '))        #defining length of list
+# nl1 = IntList(i)
+# nl1.InputDataRandom()
+# nl1.prnt()
+# j = int(input('Please, enter length of second list: '))        #defining length of list
+# nl2 = IntList(j)
+# nl2.InputDataRandom()
+# nl2.prnt()
+# print('Lets try to summ them...')
+# print(nl1 + nl2)
+
+print('\n=== Task #3 программа с классом Student ===')
+def callName():
+    name = input('Enter name of student: ')
+    return name
+
+def callAge():
+    age = int(input("Enter student's age: "))
+    return age
+
+def callGroup():
+    group = input("Enter group of student: ")
+    return group
+
+class Student:
+    def __init__(self, name = 'Ivan', age = 18, groupNumber = '10A'):
+        self.name = name
+        self.age = age
+        self.groupNumber = groupNumber
+
+    def getName(self):
+        print('Name: ', self.name)
+
+    def getAge(self):
+        print('Age:  ', self.age)
+
+    def getGroupNumber(self):
+        print('Group:', self.groupNumber)
+
+    def setNameAge(self, name, age):
+        self.name = name
+        self.age = age
+
+    def setGroupNumber(self, group):
+        self.groupNumber = group
+
+    def prnt(self):         # def call of three methods in one
+        self.getName()
+        self.getAge()
+        self.getGroupNumber()
+
+
+s1 = Student()
+s2 = Student()
+s3 = Student()
+s4 = Student()
+s5 = Student()
+
+print(' Student #1')
+s1.prnt()
+s1.setNameAge(callName(), callAge())
+s1.setGroupNumber(callGroup())
+s1.prnt()
--- webinarwork/11.08.py
+++ webinarwork/11.08.py
@@ -108,4 +108,135 @@
 
 # PART 2
 
+# class Cat:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#     def info(self):
+#         print(f"I am a cat. My name is {self.name}. I am {self.age} years old.")
+#     def make_sound(self):
+#         print("Meow")
+#
+# class Dog:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#     def info(self):
+#         print(f"I am a dog. My name is {self.name}. I am {self.age} years old.")
+#     def make_sound(self):
+#         print("Bark")
+#
+# cat1 = Cat("Kitty", 2.5)
+# dog1 = Dog("Fluffy", 4)
+#
+# animal_list = [cat1, dog1]
+#
+# for animal in (cat1, dog1): # can write for animal in (animal_list):...
+#     animal.make_sound()
+#     animal.info()
+#     animal.make_sound()
 
+
+# from math import pi
+#
+# class Shape:
+#     def __init__(self, name):
+#         self.name = name
+#     def area(self):
+#         pass
+#     def fact(self):
+#         return "I am a two-dimensional shape."
+#     def __str__(self):
+#         return self.name
+#
+# class Square(Shape):
+#     def __init__(self, length):
+#         super().__init__("Square")
+#         self.length = length
+#     def area(self):
+#         return self.length**2
+#     def fact(self):
+#         return "Squares have each angle equal to 90 degrees."
+# class Circle(Shape):
+#     def __init__(self, radius):
+#         super().__init__("Circle")
+#         self.radius = radius
+#     def area(self):
+#         return pi*self.radius**2
+#
+# a = Square(4)
+# b = Circle(7)
+# print(b)
+# print(b.fact())
+# print(a.fact())
+# print(b.area())
+
+
+# class Table:
+#     def __init__(self, l, w, h):
+#         self.length = l
+#         self.width = w
+#         self.height = h
+#
+# class DeskTable(Table):
+#     def square(self):
+#         return self.width * self.length
+#
+#
+# t1 = Table(1.5, 1.8, 0.75)
+# t2 = DeskTable(0.8, 0.6, 0.7)
+# print(t2.square())
+# t2.length = 2
+# print(t2.square())
+
+# class Table:
+#     def __init__(self, l, w, h):
+#         self.length = l
+#         self.width = w
+#         self.height = h
+# class DeskTable(Table):
+#     def square(self):
+#         return self.width * self.length
+#     def __init__(self, l, w, h, c):
+#         self.length = l
+#         self.width = w
+#         self.height = h
+#         self.color=c
+# t1 = Table(1.5, 1.8, 0.75)
+# t2 = DeskTable(0.8, 0.6, 0.7, "Blue")
+# print(t2.color) # color print(t2.square())
+
+# class Table:
+#     def __init__(self, l, w, h):
+#         self.length = l
+#         self.width = w
+#         self.height = h
+#
+# class DeskTable(Table):
+#     def square(self, monitor):
+#         print('calculate DeskTable')
+#         # return self.width * self.length
+#         return self.width * self.length #- monitor
+#
+#
+# class ComputerTable(DeskTable):
+#     def square(self, monitor=0.0):
+#         print('calculate ComputerTable')
+#         return self.width * self.length #- monitor
+#
+# t3 = ComputerTable(0.8, 0.6, 0.7)
+# print(t3.square(0.3)) # вывод: 0.18
+
+class Table:
+    def __init__(self, l, w, h):
+        self.length = l
+        self.width = w
+        self.height = h
+class KitchenTable(Table):
+    def __init__(self, l, w, h, p):
+        #Table.__init__(self, l, w, h)   # not preffered
+        super().__init__(l, w, h)       # preffered choise in THAT case
+        self.places = p
+
+
+t4 = KitchenTable(1.5, 2, 0.75, 6)
